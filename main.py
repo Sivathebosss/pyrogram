299a98d721b148e8659758158ae45199b6850cba
Sivathebosss <107598918+Sivathebosss@users.noreply.github.com>
2024-06-14 12:05:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import CallbackQuery, ReplyKeyboardMarkup
 import asyncio
-import Config as C
+import config as C
 
 
 
