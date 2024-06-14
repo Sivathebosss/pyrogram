7d9248cfdb8919fc46bf897092f17e3b49b05ff3
Sivathebosss <107598918+Sivathebosss@users.noreply.github.com>
2024-06-14 12:08:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class Config(object):
      
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-    API_ID = os.getenv("API_ID", 12345))
+    API_ID = os.getenv("API_ID", 12345)
 
     API_HASH = os.getenv("API_HASH", "")
